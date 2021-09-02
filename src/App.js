74fc15e2e6310de5e16688393e9fd3eb40c6781d
TransformativeBeing <83785114+TransformativeBeing@users.noreply.github.com>
2021-09-01 19:15:46 -0700

    Created React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <input type="submit" value="Discover" className="btn btn-warning" />
+    </div>
+  );
+}
+
+export default App;
